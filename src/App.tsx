95612b37d9b71e4b2d666d13d6623c0a7f299573
Abdul Shukkoor <abduldoha2019@gmail.com>
2023-09-07 17:45:59 +0300

    fix the issu with chakra menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -32,20 +32,22 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
-            selectedPlatform={gameQuary.platform}
-            onSelectedPlatform={(platform) =>
-              setGameQuary({ ...gameQuary, platform })
-            }
-          />
+        <Flex paddingLeft={2} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector
+              selectedPlatform={gameQuary.platform}
+              onSelectedPlatform={(platform) =>
+                setGameQuary({ ...gameQuary, platform })
+              }
+            />
+          </Box>
           <SortSelector
             sortOrder={gameQuary.sortOrder}
             onSelectSortOrder={(sortOrder) =>
               setGameQuary({ ...gameQuary, sortOrder })
             }
           />
-        </HStack>
+        </Flex>
         <GameGrid gameQuery={gameQuary}></GameGrid>
       </GridItem>
     </Grid>
